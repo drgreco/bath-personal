273950a7a6158880b67cb577db3ac287355a7eb2
Mathew Branyon <mat.branyon@gmail.com>
2012-11-30 03:27:13 -0700

    Few minor bug fixes and code corrections Also, some name changes moving away from testing with apache-ldap

--- daemon/bathd.py
+++ daemon/bathd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 ############################################################################
-# Copyright [2012] [Mathew Branyon (mat.branyon@readytalk.com)]
+# Copyright [2012] [Mathew Branyon (mat.branyon@gmail.com)]
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
